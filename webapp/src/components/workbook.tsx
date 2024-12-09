517221fac57d477a3c4a5ae3883d352e6ef8d866
francisco aloi <francisco.aloi@klara.com>
2024-12-09 09:41:48 +0100

    new components for name manager dialog and fields
    new text added to locale en_us
    
    added name manager to toolbar
    
    added model and worksheets as prop
    
    UPDATE: API for defined names
    
    NamedRange component changes
    
    NameManager dialog component changes
    
    new util formatting functions
    
    UPDATE: API for defined names
    
    new components for name manager dialog and fields
    
    new text added to locale en_us
    
    added name manager to toolbar
    
    added model and worksheets as prop
    
    UPDATE: API for defined names
    
    NamedRange component changes
    
    NameManager dialog component changes
    
    new util formatting functions
    
    UPDATE: API for defined names
    
    last changes
    
    corrected styling
    
    updates to namedRange and nameManagerDialog

--- webapp/src/components/workbook.tsx
+++ webapp/src/components/workbook.tsx
@@ -115,6 +115,10 @@ const Workbook = (props: { model: Model; workbookState: WorkbookState }) => {
     updateRangeStyle("num_fmt", numberFmt);
   };
 
+  const onNamedRangesUpdate = () => {
+    // update named ranges in model
+  };
+
   const onCopyStyles = () => {
     const {
       sheet,
@@ -569,6 +573,7 @@ const Workbook = (props: { model: Model; workbookState: WorkbookState }) => {
           model.setShowGridLines(sheet, show);
           setRedrawId((id) => id + 1);
         }}
+        model={model}
       />
       <FormulaBar
         cellAddress={cellAddress()}
